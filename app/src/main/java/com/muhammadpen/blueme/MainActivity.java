5fae7e2abce0753ca7671e1d86b59069ff55a1cb
MuhammadPEN <Splasheoo@gmail.com>
2018-01-03 16:20:20 +0500

    update 0.3
    Changed the Layout
    Added Youtube Players
    (TO FIX) the button is not aligning up properly.
    (TO ADD) youtube video functionality to every videoPlayer

--- app/src/main/java/com/muhammadpen/blueme/MainActivity.java
+++ app/src/main/java/com/muhammadpen/blueme/MainActivity.java
@@ -1,149 +1,39 @@
-//TO DO :
-//Make video view invisible after playing -done
-//Add padding between video views -done
-//Fix the splash screen -done
-//Add full screen video players with media controllers
-//Convert these into a list for addition of more video views and enable verticle scrolling
-//Use android icons and add a play-icon on every thumb nail
-//(Late development) Make it so that a user can use a media controller to enter full screen
-//Figure out smoother transition between the splash screen and the content
-//Manage this properly on Git hub.
-
 package com.muhammadpen.blueme;
 
-import android.media.Image;
-import android.media.MediaPlayer;
-import android.net.Uri;
-import android.os.CountDownTimer;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.CardView;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.MediaController;
-import android.widget.RelativeLayout;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ScrollView;
 import android.widget.VideoView;
-
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity {
-
-    //Initializing views and variables etc
-    ImageView splashScreen = null;
-    CountDownTimer splashScreenTimer;
-    VideoView card1VideoView = null;
-    VideoView card2VideoView = null;
-    VideoView card3VideoView = null;
-    CardView card1 = null;
-    CardView card2 = null;
-    CardView card3 = null;
-
-    public void card1Pressed(View view){
+//Imports for youtube
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 
-        boolean playback = false;
+public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
 
-        //Setting up views
-        card1VideoView = findViewById(R.id.card1VideoView);
-        card1 = findViewById(R.id.card1);
+    //Activating youtube functionality through API key
+    public final class Config {
 
-        //Setting up video link
-        String uriPath = "android.resource://com.muhammadpen.blueme/" + R.raw.leaves;
-        Uri uri = Uri.parse(uriPath);
-        card1VideoView.setVideoURI(uri);
-
-        if (playback == false) {
-
-            //Playing the video
-            card1.animate().alpha(0);
-            card1VideoView.animate().alpha(1);
-            card1VideoView.start();
-
-
-            card1VideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener()
-            {
-                @Override
-                public void onCompletion(MediaPlayer mp1)
-                {
-                    // Video Playing is completed
-                    card1.animate().alpha(1);
-                    card1VideoView.animate().alpha(0);
-
-                }
-            });
+        private Config() {
         }
+
+        public static final String YOUTUBE_API_KEY = "AIzaSyCR4SeoSZOZI9q2FAtdch6_Y03nX1nAlDo";
 
     }
 
-    public void card2Pressed(View view){
-
-        boolean playback = false;
-
-        //Setting up views
-        card2VideoView = findViewById(R.id.card2VideoView);
-        card2 = findViewById(R.id.card2);
-
-        //Setting up video link
-        String uriPath = "android.resource://com.muhammadpen.blueme/" + R.raw.bird;
-        Uri uri = Uri.parse(uriPath);
-        card2VideoView.setVideoURI(uri);
-
-        if (playback == false) {
-
-            //Playing the video
-            card2.animate().alpha(0);
-            card2VideoView.animate().alpha(1);
-            card2VideoView.start();
-
-            card2VideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener()
-            {
-                @Override
-                public void onCompletion(MediaPlayer mp2)
-                {
-                    // Video Playing is completed
-                    card2.animate().alpha(1);
-                    card2VideoView.animate().alpha(0);
-                }
-            });
-        }
-
-    }
-
-    public void card3Pressed(View view){
-
-
-        boolean playback = false;
-
-        //Setting up views
-        card3VideoView = findViewById(R.id.card3VideoView);
-        card3 = findViewById(R.id.card3);
-
-        //Setting up video link
-        String uriPath = "android.resource://com.muhammadpen.blueme/" + R.raw.bunny;
-        Uri uri = Uri.parse(uriPath);
-        card3VideoView.setVideoURI(uri);
-
-        if (playback == false) {
-
-            //Playing the video
-            card3.animate().alpha(0);
-            card3VideoView.animate().alpha(1);
-            card3VideoView.start();
-
-            card3VideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener()
-            {
-                @Override
-                public void onCompletion(MediaPlayer mp3)
-                {
-                    // Video Playing is completed
-                    card3.animate().alpha(1);
-                    card3VideoView.animate().alpha(0);
-                }
-            });
-        }
-
-    }
+    //Initializing the views
+    EditText searchEditText = null;
+    ImageButton searchButton = null;
+    YouTubePlayerView vid1 = null;
+    YouTubePlayerView vid2 = null;
+    YouTubePlayerView vid3 = null;
+    ScrollView scrollView = null;
+    private static final int RECOVERY_REQUEST = 1;
 
 
     @Override
@@ -151,30 +41,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Initially setting everything to be invisible
-        final RelativeLayout relativeLayout = findViewById(R.id.relativeLayout);
-        relativeLayout.setAlpha(0);
-
-        //Splash screen with fade animation
-        splashScreen = findViewById(R.id.splashScreenImageView);
-        splashScreen.bringToFront();
-        splashScreenTimer = new CountDownTimer(1000, 100) {
-            @Override
-            public void onTick(long l) {
-
-            }
-
-            @Override
-            public void onFinish() {
-
-                splashScreen.animate().alpha((float) 0.00);
-                //Everything becomes visible in a fade-in manner
-                relativeLayout.animate().alpha(1);
-            }
-        }.start();
-
+     //Defining the views
+        searchEditText = findViewById(R.id.searchEditText);
+        searchButton = findViewById(R.id.searchButton);
+        scrollView = findViewById(R.id.scrollView);
 
     }
 
+    @Override
+    public void onInitializationSuccess(Provider provider, YouTubePlayer youTubePlayer, boolean b) {
 
+    }
+
+    @Override
+    public void onInitializationFailure(Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+    }
 }
