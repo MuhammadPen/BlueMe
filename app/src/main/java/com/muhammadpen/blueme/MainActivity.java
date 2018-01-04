d194350961443c941c85b153ed45fc0fdfee4ea1
MuhammadPEN <Splasheoo@gmail.com>
2018-01-04 15:59:12 +0500

    update 0.4
    Working on layout for the home screen.
    
    (IMPORTANT)Having problem with merging dex files.

--- app/src/main/java/com/muhammadpen/blueme/MainActivity.java
+++ app/src/main/java/com/muhammadpen/blueme/MainActivity.java
@@ -4,36 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ScrollView;
-import android.widget.VideoView;
-//Imports for youtube
-import com.google.android.youtube.player.YouTubeBaseActivity;
-import com.google.android.youtube.player.YouTubeInitializationResult;
-import com.google.android.youtube.player.YouTubePlayer;
-import com.google.android.youtube.player.YouTubePlayer.Provider;
-import com.google.android.youtube.player.YouTubePlayerView;
+import com.squareup.picasso.Picasso;
 
 
-public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+public class MainActivity extends AppCompatActivity {
 
-    //Activating youtube functionality through API key
-    public final class Config {
-
-        private Config() {
-        }
-
-        public static final String YOUTUBE_API_KEY = "AIzaSyCR4SeoSZOZI9q2FAtdch6_Y03nX1nAlDo";
-
-    }
 
     //Initializing the views
     EditText searchEditText = null;
     ImageButton searchButton = null;
-    YouTubePlayerView vid1 = null;
-    YouTubePlayerView vid2 = null;
-    YouTubePlayerView vid3 = null;
+    ImageView vid1Thumbnail = null;
+    ImageView vid2Thumbnail = null;
+    ImageView vid3Thumbnail = null;
     ScrollView scrollView = null;
-    private static final int RECOVERY_REQUEST = 1;
 
 
     @Override
@@ -45,16 +30,12 @@ public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.O
         searchEditText = findViewById(R.id.searchEditText);
         searchButton = findViewById(R.id.searchButton);
         scrollView = findViewById(R.id.scrollView);
+        vid1Thumbnail = findViewById(R.id.vid1Thumbnail);
+        vid2Thumbnail = findViewById(R.id.vid2Thumbnail);
+        vid3Thumbnail = findViewById(R.id.vid3Thumbnail);
+
+        Picasso.with(this).load("https://img.youtube.com/vi/QxIR8x2CefM/0.jpg").into(vid1Thumbnail);
 
     }
 
-    @Override
-    public void onInitializationSuccess(Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-
-    }
-
-    @Override
-    public void onInitializationFailure(Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
-
-    }
 }
